32debbfebaa10dc020d78af6900cd668f8a8a53f
TrumpsFahter <1119462481@qq.com>
2019-04-30 16:55:55 +0800

    commit stack.py

---
+++ python/stack.py
@@ -0,0 +1,26 @@
+class Stack(object):
+    def __init__(self, limit = 10):
+        self.stack = []
+        self.limit = limit
+
+    def push(self, data):
+        if len(self.stack) >= self.limit:
+            print('Stack over flow Error')
+            pass
+        self.stack.append(data)
+
+    def pop(self):
+        if self.stack:
+            return self.stack.pop()
+        else:
+            raise IndexError('pop from an empty stack')
+
+    def peek(self):
+        if self.stack:
+            return self.stack[-1]
+
+    def is_empty(self):
+        return not bool(self.stack)
+
+    def size(self):
+        return len(self.stack)
